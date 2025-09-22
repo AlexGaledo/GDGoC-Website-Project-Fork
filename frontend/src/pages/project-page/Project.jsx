24b0e8fb3853bb7a1c01f719e912b52d839b89f2
Dan-code25 <danjhenielbringas1125@gmail.com>
2025-09-22 17:53:54 +0800

    added hero section and project cards

--- frontend/src/pages/project-page/Project.jsx
+++ frontend/src/pages/project-page/Project.jsx
@@ -1,10 +1,29 @@
 import Navbar from "../../components/navigation-bar/Navbar";
+import HeroText from "../../components/hero-text/Herotext";
+import ProjectCard from "../../components/project-card/ProjectCard";
 import styles from "../project-page/Project.module.css";
+import heroStyles from "../../components/hero-text/Herotext.module.css";
+import websiteProjectImg from "../../assets/images/project-images/gdgoc-website.svg";
 
 function Project(){
   return(
     <>
-     <Navbar />
+      <Navbar />
+      <HeroText 
+        header = "Explore our Project"
+        subtext = {
+          <>
+            Explore tech excellence with GDSC TUP Manila! Join us for <span className={heroStyles.highlight1}>innovative projects</span>, <span className={heroStyles.highlight2}>tech excellence</span>, <span className={heroStyles.highlight3}>vibrant events</span>, and a <span className={heroStyles.highlight4}>dynamic community</span>. Elevate your skills and be part of shaping the future together!
+          </>
+        }
+      />
+      <ProjectCard 
+        image = {websiteProjectImg}
+        title = "GDGoC TUPM - Official Website"
+        description = "Welcome to the Google Developer Student Club - TUP Manila website. Immerse yourself in a community of dedicated members, uncover a diverse array of services, and actively engage in the development of unforgettable projects!"
+        designer="Juan Dela Cruz"
+        developer="Cruz Dela Juan"
+      />
     </>
   );
 }
