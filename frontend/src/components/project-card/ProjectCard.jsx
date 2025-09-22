41bab5da6077e33529949f5e7f2c4ddb85392bad
Dan-code25 <danjhenielbringas1125@gmail.com>
2025-09-22 17:52:40 +0800

    added project card component
    reusable component for inserting projectsin project page

---
+++ frontend/src/components/project-card/ProjectCard.jsx
@@ -0,0 +1,45 @@
+import styles from "../project-card/ProjectCard.module.css";
+
+function ProjectCard({image, title, description, designer, developer}){
+  return(
+    <>
+      {/* Mobile design */}
+      <div className={`${styles.card} d-flex flex-column align-items-center d-md-none`}>
+        <img src={image} alt={title} className={styles.projectImage}/>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.description}>{description}</p>
+        <span className={styles.role}>Designers: <span className={styles.name}>{designer}</span></span>
+        <span className={styles.role}>Developers: <span className={styles.name}>{developer}</span></span>
+        <button className={styles.viewButton}>View Project</button>
+      </div>
+      {/* Tablet design */}
+      <div className={`${styles.card} d-none d-xl-none d-md-flex flex-column align-items-center`}>
+        <h1 className={styles.title}>{title}</h1>
+        <div className="d-flex justify-content-center gap-4">
+          <img src={image} alt={title} className={styles.projectImage}/>
+          <div className="d-flex flex-column align-items-start">
+            <p className={styles.description}>{description}</p>
+            <span className={styles.role}>Designers: <span className={styles.name}>{designer}</span></span>
+            <span className={styles.role}>Developers: <span className={styles.name}>{developer}</span></span>
+            <button className={styles.viewButton}>View Project</button>
+          </div>
+        </div>
+      </div>
+      {/* Desktop Design */}
+      <div className={`${styles.card} d-none d-xl-flex justify-content-center`}>
+        <div className="d-flex justify-content-center gap-4">
+          <img src={image} alt={title} className={styles.projectImage}/>
+          <div className="d-flex flex-column align-items-start">
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.description}>{description}</p>
+            <span className={styles.role}>Designers: <span className={styles.name}>{designer}</span></span>
+            <span className={styles.role}>Developers: <span className={styles.name}>{developer}</span></span>
+            <button className={styles.viewButton}>View Project</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProjectCard;
