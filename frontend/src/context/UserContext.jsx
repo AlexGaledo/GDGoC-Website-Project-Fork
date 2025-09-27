58a8ce406bab7e1aaf707aaa1a0e19bb92d2d2e0
Wen <frondairwen@gmail.com>
2025-09-27 17:30:39 +0800

    profile reflected to navbar & sidenav

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -6,18 +6,25 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [firstName, setFirstName] = useState("Alex");
-    const [lastName, setLastName] = useState("Galedo");
-    const [username, setUsername] = useState("@negahT");
+    const [firstName, setFirstName] = useState("Irwen");
+    const [lastName, setLastName] = useState("Fronda");
+    const [username, setUsername] = useState("@wen");
     const [gdgID, setGdgID] = useState(1448);
     const [expNumber, setExpNumber] = useState(1000);
     const [idPad, setIdPad] = useState("GDGTUP-25-");
-    const [profilePic, setProfilePic] = useState(null);
+    
+    const [profilePic, setProfilePic] = useState(() => {
+        const storedPic = localStorage.getItem("profilePic");
+        return storedPic && storedPic.startsWith("data:image") ? storedPic : null;
+    });
 
     useEffect(() => {
         if (profilePic) {
-            localStorage.setItem("profilePic", profilePic);
-        } 
+            localStorage.setItem("profilePic", profilePic)
+        }
+        else {
+            localStorage.removeItem("profilePic");
+        }
     }, [profilePic]);
 
     return (
