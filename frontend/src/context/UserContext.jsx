ea9b06d3acc936dea97d017963b2c67b5b68cd48
Wen <frondairwen@gmail.com>
2025-09-27 22:47:41 +0800

    to review

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -6,10 +6,10 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const [firstName, setFirstName] = useState("Irwen");
-    const [lastName, setLastName] = useState("Fronda");
-    const [username, setUsername] = useState("@wen");
-    const [gdgID, setGdgID] = useState(1448);
+    const [firstName, setFirstName] = useState("Firstname");
+    const [lastName, setLastName] = useState("Lastname");
+    const [username, setUsername] = useState("@username");
+    const [gdgID, setGdgID] = useState(1458);
     const [expNumber, setExpNumber] = useState(1000);
     const [idPad, setIdPad] = useState("GDGTUP-25-");
     
