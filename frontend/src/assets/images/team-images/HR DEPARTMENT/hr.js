e62fbedfb7e0c2bc93900ace03805898abe574bf
Dan-code25 <danjhenielbringas1125@gmail.com>
2025-10-02 00:08:32 +0800

    updated teams.jsx and added images of other department

---
+++ frontend/src/assets/images/team-images/HR DEPARTMENT/hr.js
@@ -0,0 +1,14 @@
+import Balberan from './Balberan, Jhan Leur L._Human Resources Communications Lead.png';
+import Concepcion from './Concepcion, Louise Andrea, B._+Member Development Lead.png';
+import Layam from './Layam, Ailyn C_Human Resources Communications Lead.png';
+import Sarvida from './Sarvida, Jewel R. _Department Relations Lead.png';
+
+
+const hr = {
+  Balberan,
+  Concepcion,
+  Layam,
+  Sarvida,
+}
+
+export default hr;
---
+++ frontend/src/assets/images/team-images/INNOVATIONS DEPARTMENT/innovations.js
@@ -0,0 +1,20 @@
+import Baladjay from './Chrystel G. Baladjay_Innovation Documentation Lead.png';
+import Paraiso from './Ed Mcbille C. Paraiso_Innovation Documentation Lead.png';
+import Susara from './Ethan Lee M. Susara_Innovation Documentation Lead.png';
+import Khinito from './Khinito Christian C. Peñamente_Community Engagement Lead.png';
+import Lota from './Kyle Louise A. Lota_Innovation Researcher Lead.png';
+import Lucto from './Mark Christian L. Lucto_Community Engagement Lead.png';
+import Alcantara from './Nicholas Andrew L. Alcantara_Innovation Strategy Lead.png';
+
+
+const innov = {
+  Baladjay,
+  Paraiso,
+  Susara,
+  Khinito,
+  Lota,
+  Lucto,
+  Alcantara,
+}
+
+export default innov;
